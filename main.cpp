797bf9b647e8c31cffa8f30695fb83af3cf39270
Nazmul Islam <124438941+nazmul-d3v@users.noreply.github.com>
2024-12-28 23:00:49 +0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,4 +13,26 @@ public:
     {
         a=x;
     }
+demo (demo & z )
+{
+ a=z.a;
+    
+}
+
+void putdata()
+{
+    court<<"\n a="<<a;
+}
 };
+
+int main()
+{
+    demo aa;
+    demo bb(20);
+    demo cc(aa);
+
+    aa.putdata();
+    bb.putdata();
+    cc.putdata();
+    return 0;
+}
